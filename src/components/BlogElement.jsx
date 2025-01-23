1d24976b9b5b2c6d884f8d33c59b4c685ae5782d
Garajnik <lexan1699@gmail.com>
2025-01-23 11:11:19 +0700

    Readme change

--- src/components/BlogElement.jsx
+++ src/components/BlogElement.jsx
@@ -10,16 +10,7 @@ export default function BlogElement(imageSource) {
         src="/src/assets/Blog_Images/Scaffolding_generator.png"
         alt={`${imageSource}`}
       />
-      <div className={styles.text}>
-        <h1>Scaffolding tool for Unity</h1>
-        <hr />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
-          adipisci omnis deleniti dignissimos voluptatibus impedit ullam
-          expedita ut, vero molestias nemo repudiandae, totam quae vel sit harum
-          voluptatum voluptate accusamus?
-        </p>
-      </div>
+      <div className={styles.text}></div>
     </div>
   );
 }
