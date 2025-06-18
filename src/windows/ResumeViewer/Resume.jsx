969d7038bd8df4b751bbcd829e1a8ad069db57bf
Garajnik <lexan1699@gmail.com>
2025-06-18 12:20:04 +0700

    Fix resume pic

--- src/windows/ResumeViewer/Resume.jsx
+++ src/windows/ResumeViewer/Resume.jsx
@@ -1,11 +1,8 @@
 import styles from "./Resume.module.css";
+import resume from "../../assets/Resume.png";
 
 export default function Resume() {
   return (
-    <img
-      draggable={false}
-      src="/src/assets/Resume.png"
-      className={styles.container}
-    ></img>
+    <img draggable={false} src={resume} className={styles.container}></img>
   );
 }
